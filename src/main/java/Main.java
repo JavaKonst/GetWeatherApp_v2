d5ad6b29bd7bd6ad6381a58ce699abfc6e2dae3d
konstNBH <kkk@mail.ru>
2019-12-02 22:10:00 +0500

    v1.1 Fix some error message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,33 @@
 //Основные действия
 
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) {
         String city = "Москва";
         String[] lon_lat;
         String temp;
-        int numService = 1;
+        int numService;
         String isSave;
         String isStop = "да";
         Scanner scanner = new Scanner(System.in);
 
-
         while (isStop.equalsIgnoreCase("да")) {
-
-
             System.out.print("Введите город: ");
-            if(scanner.hasNext()) city = scanner.nextLine();
+            if (scanner.hasNext()) city = scanner.nextLine();
+
+            LatLonCity latLonCity = new LatLonCity();
+            lon_lat = latLonCity.getLatLon(city);
+            if (lon_lat[0].contains("Ошибка")) {
+                System.out.println(lon_lat[0]);
+                System.out.print("\nПродолжить (да/нет)?  --> ");
+                if (scanner.hasNext()) isStop = scanner.nextLine();
+                System.out.println("\n------------------\n\n");
+                continue;
+            }
+
+            city = latLonCity.getFullCityName();
 
             System.out.print("Выберите погодный сервис:" +
                     "\n\t1) Yandex.Weather" +
@@ -27,28 +35,21 @@ public class Main {
                     "\n\t3) OpenWeather\n");
             System.out.print("--> ");
 
-            if(scanner.hasNext()) numService = scanner.nextInt();
-
-            LatLonCity latLonCity = new LatLonCity();
-            lon_lat = latLonCity.getLatLon(city);
-            if (lon_lat[0].contains("Ошибка")) {
-                System.out.println(lon_lat[0]);
-                System.out.print("\nПродолжить (да/нет)?  --> ");
-                scanner.nextLine();
-                if(scanner.hasNext()) isStop = scanner.nextLine();
-                System.out.println("\n------------------\n\n");
-                continue;
+            String in = "";
+            while (true) {
+                if (scanner.hasNext()) in = scanner.nextLine();
+                if (in.equals("1") || in.equals("2") || in.equals("3")) break;
+                else System.out.print("Ошибка! Повторите свой выбор:\n--> ");
             }
 
-            city = latLonCity.getFullCityName();
+            numService = Integer.parseInt(in);
 
             temp = new WeatherServices().getCityTemp(lon_lat[1], lon_lat[0], numService);
 
             if (temp.contains("Ошибка")) {
                 System.out.println(temp);
                 System.out.print("\nПродолжить (да/нет)?  --> ");
-                scanner.nextLine();
-                if(scanner.hasNext()) isStop = scanner.nextLine();
+                if (scanner.hasNext()) isStop = scanner.nextLine();
                 System.out.println("\n------------------\n\n");
                 continue;
             }
@@ -58,16 +59,14 @@ public class Main {
             if (isSave.contains("Ошибка")) {
                 System.out.println(isSave);
                 System.out.print("\nПродолжить (да/нет)?  --> ");
-                scanner.nextLine();
-                if(scanner.hasNext()) isStop = scanner.nextLine();
+                if (scanner.hasNext()) isStop = scanner.nextLine();
                 System.out.println("\n------------------\n\n");
                 continue;
             }
-            System.out.println("ОТВЕТ: Температура в городе " + city + ": " + temp + "\n\n");
-            
+            System.out.println("ОТВЕТ: Температура в городе " + city + ": " + temp + "\n");
+
             System.out.print("Продолжить (да/нет)?  --> ");
-            scanner.nextLine();
-            if(scanner.hasNext()) isStop = scanner.nextLine();
+            if (scanner.hasNext()) isStop = scanner.nextLine();
             System.out.println("\n------------------\n");
         }
     }
