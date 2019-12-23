4aaf7d66c4f2001855cc283f35effcb1a1c8751c
konstNBH <kkk@mail.ru>
2019-12-23 22:39:02 +0500

    Add parsing json (yandex, accu)

--- src/main/java/LatLonCity.java
+++ src/main/java/LatLonCity.java
@@ -1,6 +1,5 @@
 //Получение координат(широта и долгота) введенного города в сервисе Yandex.Geocoder
 
-import jdk.nashorn.internal.parser.JSONParser;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -9,10 +8,6 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.json.JSONPointer;
-import org.json.JSONString;
-
-import java.util.Arrays;
 
 public class LatLonCity {
     private String cityInput;
@@ -37,12 +32,12 @@ public class LatLonCity {
         String accessKey = "2bd70c9e-0d97-4e45-b9fb-e1cec797488e";                            //Временный ключ доступа к сервису, тариф Тестовый
         String geocoderURL = "https://geocode-maps.yandex.ru/1.x/";
 
-        //Формирование запроса для получения широты и долготы города
+        //Формирование запроса для получения широты и долготы города в формате Json
         HttpGet request = new HttpGet(geocoderURL + "?format=json&apikey=" + accessKey + "&geocode=" + cityInput);
 
         try (CloseableHttpResponse response = httpClient.execute(request)) {
             if (response.getStatusLine().getStatusCode() != 200) {
-                errorMsg = "Ошибка! Сервис YandexGeocoder не отвечает.";
+                errorMsg = "Ошибка! Сервис YandexGeocoder выдал код ответа: "+ response.getStatusLine().getStatusCode() + ".";
                 return false;
             }
 
@@ -50,78 +45,37 @@ public class LatLonCity {
 
             if (entity != null) {
                 String data = EntityUtils.toString(entity);
-                String findedCity;
-
-                System.out.println(data);
 
                 JSONObject object = new JSONObject(data);
-
-                JSONObject step0 = object.getJSONObject("response");
-                JSONObject step1 = step0.getJSONObject("GeoObjectCollection");
-                JSONArray step2 = (JSONArray) step1.get("featureMember");
-                JSONObject step3 = (JSONObject) step2.get(0);
-                JSONObject step4 = step3.getJSONObject("GeoObject");
-                System.out.println(step4.getString("name")+" ("+step4.getString("description")+")");
-                //String data1 = "{\"response\":{\"GeoObjectCollection\":{\"metaDataProperty\":{\"GeocoderResponseMetaData\":{\"request\":\"минск\",\"results\":\"10\",\"found\":\"4\"}},\"featureMember\":[{\"GeoObject\":{\"metaDataProperty\":{\"GeocoderMetaData\":{\"precision\":\"other\",\"text\":\"Беларусь, Минск\",\"kind\":\"locality\",\"Address\":{\"country_code\":\"BY\",\"formatted\":\"Беларусь, Минск\",\"Components\":[{\"kind\":\"country\",\"name\":\"Беларусь\"},{\"kind\":\"province\",\"name\":\"Минск\"},{\"kind\":\"locality\",\"name\":\"Минск\"}]},\"AddressDetails\":{\"Country\":{\"AddressLine\":\"Беларусь, Минск\",\"CountryNameCode\":\"BY\",\"CountryName\":\"Беларусь\",\"AdministrativeArea\":{\"AdministrativeAreaName\":\"Минск\",\"Locality\":{\"LocalityName\":\"Минск\"}}}}}},\"name\":\"Минск\",\"description\":\"Беларусь\",\"boundedBy\":{\"Envelope\":{\"lowerCorner\":\"27.374416 53.79388\",\"upperCorner\":\"28.080303 53.97162\"}},\"Point\":{\"pos\":\"27.561481 53.902496\"}}},{\"GeoObject\":{\"metaDataProperty\":{\"GeocoderMetaData\":{\"precision\":\"other\",\"text\":\"Россия, Красноярский край, Большемуртинский район, деревня Минск\",\"kind\":\"locality\",\"Address\":{\"country_code\":\"RU\",\"formatted\":\"Россия, Красноярский край, Большемуртинский район, деревня Минск\",\"Components\":[{\"kind\":\"country\",\"name\":\"Россия\"},{\"kind\":\"province\",\"name\":\"Сибирский федеральный округ\"},{\"kind\":\"province\",\"name\":\"Красноярский край\"},{\"kind\":\"area\",\"name\":\"Большемуртинский район\"},{\"kind\":\"locality\",\"name\":\"деревня Минск\"}]},\"AddressDetails\":{\"Country\":{\"AddressLine\":\"Россия, Красноярский край, Большемуртинский район, деревня Минск\",\"CountryNameCode\":\"RU\",\"CountryName\":\"Россия\",\"AdministrativeArea\":{\"AdministrativeAreaName\":\"Красноярский край\",\"SubAdministrativeArea\":{\"SubAdministrativeAreaName\":\"Большемуртинский район\",\"Locality\":{\"LocalityName\":\"деревня Минск\"}}}}}}},\"name\":\"деревня Минск\",\"description\":\"Большемуртинский район, Красноярский край, Россия\",\"boundedBy\":{\"Envelope\":{\"lowerCorner\":\"93.326029 57.095468\",\"upperCorner\":\"93.348029 57.101213\"}},\"Point\":{\"pos\":\"93.336952 57.098304\"}}},{\"GeoObject\":{\"metaDataProperty\":{\"GeocoderMetaData\":{\"precision\":\"other\",\"text\":\"Беларусь, Минск, Национальный аэропорт Минск\",\"kind\":\"airport\",\"Address\":{\"country_code\":\"BY\",\"formatted\":\"Беларусь, Минск, Национальный аэропорт Минск\",\"Components\":[{\"kind\":\"country\",\"name\":\"Беларусь\"},{\"kind\":\"province\",\"name\":\"Минск\"},{\"kind\":\"airport\",\"name\":\"Национальный аэропорт Минск\"}]},\"AddressDetails\":{\"Country\":{\"AddressLine\":\"Беларусь, Минск, Национальный аэропорт Минск\",\"CountryNameCode\":\"BY\",\"CountryName\":\"Беларусь\",\"AdministrativeArea\":{\"AdministrativeAreaName\":\"Минск\",\"Locality\":{\"DependentLocality\":{\"DependentLocalityName\":\"Национальный аэропорт Минск\"}}}}}}},\"name\":\"Национальный аэропорт Минск\",\"description\":\"Минск, Беларусь\",\"boundedBy\":{\"Envelope\":{\"lowerCorner\":\"27.999625 53.86235\",\"upperCorner\":\"28.077904 53.912929\"}},\"Point\":{\"pos\":\"28.033824 53.889216\"}}},{\"GeoObject\":{\"metaDataProperty\":{\"GeocoderMetaData\":{\"precision\":\"street\",\"text\":\"Молдова, Кишинёв, улица Минск\",\"kind\":\"street\",\"Address\":{\"country_code\":\"MD\",\"formatted\":\"Молдова, Кишинёв, улица Минск\",\"Components\":[{\"kind\":\"country\",\"name\":\"Молдова\"},{\"kind\":\"area\",\"name\":\"муниципий Кишинёв\"},{\"kind\":\"locality\",\"name\":\"Кишинёв\"},{\"kind\":\"street\",\"name\":\"улица Минск\"}]},\"AddressDetails\":{\"Country\":{\"AddressLine\":\"Молдова, Кишинёв, улица Минск\",\"CountryNameCode\":\"MD\",\"CountryName\":\"Молдова\",\"AdministrativeArea\":{\"AdministrativeAreaName\":\"муниципий Кишинёв\",\"Locality\":{\"LocalityName\":\"Кишинёв\",\"Thoroughfare\":{\"ThoroughfareName\":\"улица Минск\"}}}}}}},\"name\":\"улица Минск\",\"description\":\"Кишинёв, Молдова\",\"boundedBy\":{\"Envelope\":{\"lowerCorner\":\"28.860759 46.997519\",\"upperCorner\":\"28.87038 47.001803\"}},\"Point\":{\"pos\":\"28.865529 46.999738\"}}}]}}}";
-
-//                JSONPointer pointer = new JSONPointer("/response");
-
-//                JSONObject response1 = object.getJSONObject("response");
-//                JSONObject geoObjectCollection = response1.getJSONObject("GeoObjectCollection");
-//
-//                JSONArray featureMember = geoObjectCollection.getJSONArray("featureMember");
-//                System.out.println(featureMember.length()+"-------"+featureMember.toString()); //4
-
-//                JSONObject geoObject = featureMember.getJSONObject(0);
-//                System.out.println(geoObject.names().toString());
-//                System.out.println(featureMember.getJSONObject(1).names().toString());
-//
-//                System.out.println("");
-//                System.out.println(featureMember.get(0).toString());
-//                System.out.println("");
-//
-//                JSONObject geoObject = featureMember.getJSONObject(0);
-////                JSONArray array = geoObject.getJSONArray("metaDataProperty");
-////                System.out.println(array.toString());
-//                String str = geoObject.getString("name");
-//                System.out.println(str);
-
-
-//                JSONObject meta = geoObject.getJSONObject("metaDataProperty");
-//                JSONObject geocoder = meta.getJSONObject("GeocoderMetaData");
-
-//                JSONObject metaDataProperty = featureMember..getJSONObject("metaDataProperty");
-//                JSONObject geocoderMetaData = metaDataProperty.getJSONObject("GeocoderMetaData");
-//                String request1 = geocoderMetaData.getString("text");
-//                System.out.println("---: "+ request1);
-
-
-//                System.out.println("== " + gson.toString());
-//                System.out.println("== "+dataJson.toString());
-                while (entity != null){
-                 Thread.sleep(1000);
-                }
+                JSONObject jsonResponse = object.getJSONObject("response");
+                JSONObject jsonGeoObjCol = jsonResponse.getJSONObject("GeoObjectCollection");
+                JSONObject jsonMetaDataProp = jsonGeoObjCol.getJSONObject("metaDataProperty");
+                JSONObject jsonGeocoderMetaData = jsonMetaDataProp.getJSONObject("GeocoderResponseMetaData");
+                String foundedItems = jsonGeocoderMetaData.getString("found");
 
                 //Если найдено записей 0, то выход из функции с ошибкой
-                if (Integer.parseInt(data.substring(data.indexOf("<found>") + 7, data.indexOf("</found>"))) == 0) {
+                if (foundedItems.equals("0")) {
                     errorMsg = "Ошибка! Город не найден.";
                     return false;
                 }
+
+                JSONArray jsonFeatureMember = (JSONArray) jsonGeoObjCol.get("featureMember");
+                JSONObject jsonNum0 = (JSONObject) jsonFeatureMember.get(0);
+                JSONObject jsonGeoObj = jsonNum0.getJSONObject("GeoObject");
+                JSONObject jsonPoint = jsonGeoObj.getJSONObject("Point");
+                String point = jsonPoint.getString("pos");
+                cityOutput = jsonGeoObj.getString("name");
 
                 //Если найденный город не совпадает с введенным, то выход из функции с ошибкой
-                findedCity = data.substring(data.indexOf("<formatted>") + 11, data.indexOf("</formatted>"));
-                if (!cityInput.equalsIgnoreCase(findedCity.substring(findedCity.lastIndexOf(" ") + 1))) {
+                if (!cityInput.equalsIgnoreCase(cityOutput)) {
                     errorMsg = "Ошибка! Город не найден.";
                     return false;
                 }
 
-                //Форматирование строки: "Город (Cтрана)"
-                String[] cityNames = findedCity.split(", ");
-                cityOutput = Character.toUpperCase(cityOutput.charAt(0)) + cityOutput.substring(1) + " (" + cityNames[0] + ")";
+                cityOutput = cityOutput + " ("+jsonGeoObj.getString("description")+")";
 
                 //Поиск и получение координат города
-                data = data.substring(data.indexOf("<pos>") + 5, data.indexOf("</pos>"));
-                lon_lat = data.split(" ");
+                lon_lat = point.split(" ");
             } else {
                 errorMsg = "Ошибка! Сервис YandexGeocoder выдал пустые данные.";
                 return false;
--- src/main/java/WeatherServices.java
+++ src/main/java/WeatherServices.java
@@ -1,4 +1,3 @@
-//Сюда нужно вводить новые сервисы???
 
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -6,6 +5,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 
 public class WeatherServices {
     private CloseableHttpClient httpClient = HttpClients.createDefault();
@@ -13,11 +13,11 @@ public class WeatherServices {
     private String cityTemp;
     private String lat;
     private String lon;
-    
+
     public String getCityTemp(String cityLAT, String cityLON, int numService) {
         lat = cityLAT;
         lon = cityLON;
-        
+
         switch (numService) {
             case 1:
                 if (!yandexWeather()) return errorMsg;
@@ -28,41 +28,41 @@ public class WeatherServices {
             case 3:
                 if (!openWeather()) return errorMsg;
                 break;
-            
+
             default:
                 return "Ошибка! Сервис не найден. Выберите другой.";
         }
-        
+
         return cityTemp;
-        
-        
+
     }
-    
+
     public boolean yandexWeather() {
         String weatherURL = "https://api.weather.yandex.ru/v1/forecast";
         String accessKey = "a9dac15c-cc86-462c-afce-7db15f8fb962";                 //Временный ключ доступа к сервису
-        
+
         //Формирование запроса: координаты города (долгота и широта), русский язык. Плюс добавление хэдэра с ключом доступа
         HttpGet request = new HttpGet(weatherURL + "?lat=" + lat + "&lon=" + lon + "&lang=ru_RU");
         request.addHeader("X-Yandex-API-Key", accessKey);
-        
+
         try (CloseableHttpResponse response = httpClient.execute(request)) {
             if (response.getStatusLine().getStatusCode() != 200) {
-                errorMsg = "Ошибка! Сервис YandexWeather не отвечает.";
+                errorMsg = "Ошибка! Сервис YandexWeather выдал код ответа: " + response.getStatusLine().getStatusCode() + ".";
                 return false;
             }
-            
+
             HttpEntity entity = response.getEntity();
-            
+
             if (entity != null) {
                 String data = EntityUtils.toString(entity);
-                
-                int tempStart = data.indexOf("temp") + 6;
-                int tempEnd = data.indexOf("feels") - 2;
-                
-                data = data.substring(tempStart, tempEnd);
-                if (Integer.parseInt(data) > 0) cityTemp = "+" + data + "\u00B0C";
-                else cityTemp = data + "\u00B0C";
+                int temp;
+
+                JSONObject object = new JSONObject(data);
+                JSONObject fact = object.getJSONObject("fact");
+                temp = fact.getInt("temp");
+
+                if (temp > 0) cityTemp = "+" + temp + "\u00B0C";
+                else cityTemp = temp + "\u00B0C";
             } else {
                 errorMsg = "Ошибка! Сервис YandexWeather выдал пустые данные.";
                 return false;
@@ -73,28 +73,32 @@ public class WeatherServices {
         }
         return true;
     }
-    
+
     public boolean accuWeather() {
         String cityIdURL = "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search";
         String weatherURL = "http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/";
         String accessKey = "nNrlkATNJUU5AB2Qj9iHm6wmC3MMrEDC";
         String cityID;
-        
+
         //Формирование запроса для получения уникального номера города cityID
         HttpGet request = new HttpGet(cityIdURL + "?apikey=" + accessKey + "&q=" + lat + "%2C%20" + lon + "&language=ru&toplevel=true");
-        
+
         try (CloseableHttpResponse response = httpClient.execute(request)) {
             if (response.getStatusLine().getStatusCode() != 200) {
-                errorMsg = "Ошибка! Сервис AccuWeather не отвечает.";
+                errorMsg = "Ошибка! Сервис AccuWeather выдал код ответа: " + response.getStatusLine().getStatusCode() + ".";
                 return false;
             }
-            
+
             HttpEntity entity = response.getEntity();
-            
+
             //Обработка полученных данных
             if (entity != null) {
                 String data = EntityUtils.toString(entity);
-                cityID = data.substring(data.indexOf("Key") + 6, data.indexOf("Type") - 3);
+
+                JSONObject object = new JSONObject(data);
+                cityID = object.getString("Key");
+
+//                cityID = data.substring(data.indexOf("Key") + 6, data.indexOf("Type") - 3);
             } else {
                 errorMsg = "Ошибка! Сервис AccuWeather выдал пустые данные.";
                 return false;
@@ -103,20 +107,35 @@ public class WeatherServices {
             errorMsg = "Ошибка! Что-то пошло не так при работе с AccuWeather: " + e.getMessage();
             return false;
         }
-        
+
         //Формирование запроса для получения информации о погоде в городе под номером cityID
         request = new HttpGet(weatherURL + cityID + "?apikey=" + accessKey + "&q=" + lat + "%2C%20" + lon + "&language=RU&metric=TRUE");
-        
+
         try (CloseableHttpResponse response = httpClient.execute(request)) {
+            if (response.getStatusLine().getStatusCode() != 200) {
+                errorMsg = "Ошибка! Сервис AccuWeather выдал код ответа: " + response.getStatusLine().getStatusCode() + ".";
+                return false;
+            }
+
             HttpEntity entity = response.getEntity();
-            
+
             //Обработка полученных данных
             if (entity != null) {
                 String data = EntityUtils.toString(entity);
+
+                data = data.substring(1,data.length()-1);
+
+                JSONObject object = new JSONObject(data);
+                JSONObject temperature = object.getJSONObject("Temperature");
+                int temp = temperature.getInt("Value");
+
                 data = data.substring(data.indexOf("Value") + 7, data.indexOf("Unit") - 2);
                 data = data.substring(0, data.indexOf("."));
                 if (Integer.parseInt(data) > 0) cityTemp = "+" + data + "\u00B0C";
                 else cityTemp = data + "\u00B0C";
+                if (temp > 0) cityTemp = "+" + temp + "\u00B0C";
+                else cityTemp = temp + "\u00B0C";
+
             } else {
                 errorMsg = "Ошибка! Сервис AccuWeather выдал пустые данные.";
                 return false;
@@ -125,24 +144,24 @@ public class WeatherServices {
             errorMsg = "Ошибка! Что-то пошло не так при работе с AccuWeather: " + e.getMessage();
             return false;
         }
-        
+
         return true;
     }
-    
+
     public boolean openWeather() {
         String weatherURL = "http://api.openweathermap.org/data/2.5/weather";
         String accessKey = "f5838bdccfef720169d8613fe0f8d0ad";
-        
+
         //Формирование запроса: координаты города (долгота и широта), ключ доступа, формат вывода xml, единицы Градусы, русский язык
         HttpGet request = new HttpGet(weatherURL + "?lat=" + lat + "&lon=" + lon + "&APPID=" + accessKey + "&mode=xml&units=metric&lang=ru");
-        
+
         try (CloseableHttpResponse response = httpClient.execute(request)) {
             if (response.getStatusLine().getStatusCode() != 200) {
                 errorMsg = "Ошибка! Сервис OpenWeather не отвечает.";
                 return false;
             }
             HttpEntity entity = response.getEntity();
-            
+
             //Обработка полученных данных
             if (entity != null) {
                 String data = EntityUtils.toString(entity);
@@ -159,7 +178,7 @@ public class WeatherServices {
             errorMsg = "Ошибка! Что-то пошло не так при работе с OpenWeather: " + e.getMessage();
             return false;
         }
-        
+
         return true;
     }
 }
